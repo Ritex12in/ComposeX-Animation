842c243b919095a362103e1b6f3b72c6f50b6f3d
Ritex12in <riteshsk001@gmail.com>
2025-04-27 19:06:37 +0530

    changing expand animation

--- app/src/main/java/com/ritesh/animatexcompose/presentaions/details/demos/ExpandAnimationPreview.kt
+++ app/src/main/java/com/ritesh/animatexcompose/presentaions/details/demos/ExpandAnimationPreview.kt
@@ -1,23 +1,18 @@
 package com.ritesh.animatexcompose.presentaions.details.demos
 
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,9 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.ritesh.animatexcompose.presentaions.ui.theme.AnimateXComposeTheme
 
 @Composable
 fun ExpandAnimationPreview() {
@@ -38,26 +32,26 @@ fun ExpandAnimationPreview() {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
+        Card (
+            colors = CardDefaults.cardColors(
+                containerColor = Color(0xFFa7c957)
+            ),
+            modifier = Modifier.padding(16.dp)) {
             Text(
                 "In a parallel universe where coffee brews itself and code writes developers, " +
-                        "a rubber duck named Jeff accidentally discovered the meaning of life in a misplaced semicolon.",
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            AnimatedVisibility(
-                visible = expanded,
-                enter = fadeIn() + expandVertically(animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy)),
-                exit = fadeOut() + shrinkVertically(animationSpec = spring(stiffness = Spring.StiffnessMedium))
-            ) {
-                Text(
-                    "Ever since that moment, Jeff’s fame skyrocketed—he now tours the cloud, giving motivational " +
-                            "quacks while it rains emojis from the newly updated sky software.",
-                    style = MaterialTheme.typography.bodyMedium
+                        "a rubber duck named Jeff accidentally discovered the meaning of life in a misplaced semicolon. "+
+                        "Ever since that moment, Jeff’s fame skyrocketed—he now tours the cloud, giving motivational " +
+                        "quacks while it rains emojis from the newly updated sky software.",
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = if (expanded) Int.MAX_VALUE else 4,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .animateContentSize(
+                    animationSpec = spring(dampingRatio = Spring.DampingRatioLowBouncy, stiffness = Spring.StiffnessLow)
                 )
-            }
+            )
         }
-        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.height(20.dp))
         Button(
             onClick = {expanded = !expanded},
             shape = RoundedCornerShape(8.dp)
